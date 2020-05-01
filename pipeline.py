289901f0eecc8155615330e9845ca2f9b6ec2a9c
Samson Weiner <samson.weiner@uconn.edu>
2020-05-01 15:11:35 -0400

    usage of pipeline correctly

--- pipeline.py
+++ pipeline.py
@@ -3,22 +3,20 @@ import sys
 from mad_results import mad_results
 from bestDTLRoot import findBestDTLRooting
 
-def runPipeline(geneTreePath, speciesTreePath):
-    print("Welcome to Briar-Rooting!")
+#geneTreePath = str(sys.argv[1])
+#speciesTreePath = str(sys.argv[2])
+print("Welcome to Briar-Rooting!")
 
-    #geneTreePath = str(sys.argv[1])
-    #speciesTreePath = str(sys.argv[2])
+print('Gene Tree:', geneTreePath)
+print('Species Tree:', speciesTreePath)
 
-    print('Gene Tree:', geneTreePath)
-    print('Species Tree:', speciesTreePath)
+print("Compiling Mad Scores...")
 
-    print("Compiling Mad Scores...")
+target_trees = mad_results(geneTreePath)
 
-    target_trees = mad_results(geneTreePath)
+print("Done.")
 
-    print("Done.")
+print("Number of trees to check DTL Scores: ", len(target_trees) )
+print("Compiling DTL scores...")
 
-    print("Number of trees to check DTL Scores: ", len(target_trees) )
-    print("Compiling DTL scores...")
-
-    optimalGeneTrees = findBestDTLRooting(speciesTreePath, target_trees)
+optimalGeneTrees = findBestDTLRooting(speciesTreePath, target_trees)
